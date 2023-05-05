6eb2d9d0382c082175885a254836e689b645c00c
KidusGR <kidmen10@gmail.com>
2023-05-05 18:16:25 +0300

    Update

---
+++ Unknown.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+#Just a test
+
+import os
+import time
+
+
+alpha = "abcdefghijklmnopqrstuvwxyz"
+check = 0
+
+def adder(word):
+	if word.lower() not in words:
+		with open("./Data/words", "a") as file:
+			file.write(f"{word.lower()}\n")
+		os.system('clear')
+		print("word added to db\n")
+	else:
+		os.system('clear')
+		print("word already in db\n")
+
+def exit():
+	os.system('clear')
+	print("G00D-!3Y3")
+	time.sleep(1)
+
+def printer():
+	os.system('clear')
+	print(f"db : {words}\n")
+
+while True:
+	words = [d.strip() for d in open("./Data/words", "r").readlines()]
+	
+	print("'exit()' -- exit\n'print()' -- print words(db)\n")
+	check = 0
+	word = input("Enter the Word: ")
+	if word == 'exit()':
+		exit()
+		break
+	elif word == 'print()':
+		printer()
+		continue
+	for w in word:
+		if w.upper() in alpha.upper():
+			pass
+		else:
+			os.system('clear')
+			print("invalid input!\n")
+			check = 1
+	if check:
+		continue
+
+	adder(word)
+	
