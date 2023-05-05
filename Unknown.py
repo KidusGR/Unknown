61c70a9664dd90076b2a130b29bf6747b96ad958
KidusGR <kidmen10@gmail.com>
2023-05-06 01:19:55 +0300

    Update

--- Unknown.py
+++ Unknown.py
@@ -4,48 +4,76 @@
 from spellchecker import SpellChecker
 import os
 import time
+from rich.console import Console
+from rich.text import Text
 
 
+console = Console()
 spell = SpellChecker()
 alpha = "abcdefghijklmnopqrstuvwxyz"
 check = 0
 
+def printBanner():
+	banner = Text(("-"*40)+"\n\tUNKNOWN WORD BARN\n"+("-"*40))
+	commands = Text("\n'exit()' -- exit\n'print()' -- print words(db)\n")
+	banner.stylize("bold magenta")
+	commands.stylize("bold blue")
+	console.print(banner+commands)
+
 def spellcheck(word):
-	global spell
-	'''print(spell.candidates(word))
-	print(spell.unknown(word))
-	print(spell[word])'''
+	line = Text(f"spelling incorrect\nsuggestions : {spell.candidates(word)}\n")
+	line.stylize("bold red")
 	os.system('clear')
 	if word in spell:
 		adder(word)
 	else:
-		print(f"spelling incorrect\nsuggestions : {spell.candidates(word)}\n")
+		console.print(line)
+
+def printInvalid():
+	line = Text("invalid input!\n")
+	line.stylize("bold red")
+	os.system('clear')
+	console.print(line)
+	
 
 def adder(word):
 	if word.lower() not in words:
 		with open("./Data/words", "a") as file:
 			file.write(f"{word.lower()}\n")
 		os.system('clear')
-		print("word added to db\n")
+		addedLine = Text("word added to db\n")
+		addedLine.stylize("bold green")
+		console.print(addedLine)
 	else:
 		os.system('clear')
-		print("word already in db\n")
+		alreadyLine = Text("word already in db\n")
+		alreadyLine.stylize("bold yellow")
+		console.print(alreadyLine)
 
 def exit():
 	os.system('clear')
-	print("G00D-!3Y3")
+	line = Text(("-"*30)+"\n\tG00D-!3Y3\n"+("-"*30))
+	line.stylize("bold magenta")
+	console.print(line)
 	time.sleep(1)
 
 def printer():
 	os.system('clear')
-	print(f"db : {words}\n")
+	line = Text(f"db : {words}\n")
+	line.stylize("bold yellow")
+	console.print(line)
 
 while True:
 	words = [d.strip() for d in open("./Data/words", "r").readlines()]
 	
-	print("'exit()' -- exit\n'print()' -- print words(db)\n")
+	printBanner()
 	check = 0
-	word = input("Enter the Word: ")
+	consoleLine = Text("Enter the Word >> ")
+	consoleLine.stylize("blue")
+	consoleLine.stylize("bold yellow", 0, -3)
+	
+	console.print(consoleLine, end="")
+	word = input()
 	if word == 'exit()':
 		exit()
 		break
@@ -56,8 +84,7 @@ while True:
 		if w.upper() in alpha.upper():
 			pass
 		else:
-			os.system('clear')
-			print("invalid input!\n")
+			printInvalid()
 			check = 1
 	if check:
 		continue
