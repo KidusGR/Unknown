da2de1754b92baf931f2a4ea0d84b4832f3dd72a
KidusGR <kidmen10@gmail.com>
2023-05-08 21:19:17 +0300

    Update

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 #Install Unknown
 
 username=$(whoami)
-echo "$username" > username
+
 sudo pip3 install -r ./requirments.txt
 sudo mkdir -p /home/$username/.UnknownData/ && sudo touch /home/$username/.UnknownData/words && sudo chmod u+x ./Unknown.py && sudo cp Unknown.py /usr/local/bin/Unknown && echo -e "\n\nNow just run\n$~ Unknown\n"
-
+echo "$username" > /home/$username/.UnknownData/username
